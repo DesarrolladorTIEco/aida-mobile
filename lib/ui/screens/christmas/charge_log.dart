b024c5b1151fedbcc5e32bc2b05d8ed393e681ff
Omar Gabriel Aguilar <desarrolladorti2@ecosac.com.pe>
2024-11-18 14:28:52 -0500

    trabajar reporte de canastas

--- lib/ui/screens/christmas/charge_log.dart
+++ lib/ui/screens/christmas/charge_log.dart
@@ -10,12 +10,32 @@ class ChageScreen extends StatefulWidget {
 }
 
 class _ChageScreenState extends State<ChageScreen> {
-  int _selectedIndex = 0;
+  // Lista de nombres
+  final List<String> _names = [
+    "Joys Navarro Adanaque",
+    "Jhon Soto Navarro",
+    "Jackson Alfaro Correa",
+    "Marlon Chira Correa",
+    "María Pérez Gonzales",
+    "Marco Soto Gonzales",
+    "Jackson Adanaque Pérez",
+    "Carlos Ramírez Salas"
+  ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+  DateTime? _selectedDate;
+
+  void _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
   }
 
   @override
@@ -25,33 +45,115 @@ class _ChageScreenState extends State<ChageScreen> {
         preferredSize: const Size.fromHeight(60.0),
         child: const TopNavBarScreen(),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
               "Reporte de Canasta entregas",
               style: GoogleFonts.raleway(
-                fontSize: 18,
+                fontSize: 16,
                 letterSpacing: 0.22,
-                fontWeight: FontWeight.w700,
+                fontWeight: FontWeight.w600,
                 color: Colors.black,
               ),
             ),
-          ),
-          const SizedBox(height: 10),
+            const SizedBox(height: 10),
 
-          // Layout con Row para los Cards y botones
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+            // DatePicker como combobox
+            GestureDetector(
+              onTap: () => _selectDate(context),
+              child: Container(
+                height: 50,
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? "Seleccionar Fecha"
+                          : "${_selectedDate!.year}-${_selectedDate!.month.toString().padLeft(2, '0')}-${_selectedDate!.day.toString().padLeft(2, '0')}",
+                      style: GoogleFonts.raleway(fontSize: 14),
+                    ),
+                    const Icon(Icons.calendar_today, color: Colors.grey),
+                  ],
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // TextField para escanear código QR
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Escanear código",
+                labelStyle: GoogleFonts.raleway(fontSize: 14, fontWeight: FontWeight.w600),
+                prefixIcon: const Icon(Icons.qr_code_scanner),
+                prefixIconConstraints: const BoxConstraints(
+                  minWidth: 40,
+                  minHeight: 40,
+                ),
+                border: const OutlineInputBorder(),
+                contentPadding: const EdgeInsets.symmetric(vertical: 22.0, horizontal: 16.0),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // Alineación a la izquierda
               children: [
+                Text(
+                  "Entregados: 40",
+                  style: GoogleFonts.raleway(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 8), // Espacio entre las dos líneas
+                Text(
+                  "Saldo: 20",
+                  style: GoogleFonts.raleway(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                ),
               ],
             ),
-          ),
-        ],
+
+            const SizedBox(height: 20),
+
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.only(top: 0), // Opcional para ajustar el espaciado
+                itemCount: _names.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 2,
+                    margin: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: ListTile(
+                      leading: const Icon(Icons.person, size: 40, color: Colors.grey),
+                      title: Text(
+                        _names[index],
+                        style: GoogleFonts.raleway(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: const BottomNavBarScreen(), // Usando BottomNavBarScreen
     );
