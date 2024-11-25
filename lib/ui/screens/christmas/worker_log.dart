2c9630942a2d1170987a4e5e164defd1049bf06e
Omar Gabriel Aguilar <desarrolladorti2@ecosac.com.pe>
2024-11-25 14:22:10 -0500

    funcionando la vista de chage_log

--- lib/ui/screens/christmas/worker_log.dart
+++ lib/ui/screens/christmas/worker_log.dart
@@ -1,6 +1,10 @@
+import 'package:aida/core/utils/scanner_qr.dart';
+import 'package:aida/viewmodel/christmas/worker_viewmodel.dart';
 import 'package:flutter/material.dart';
 import '../../widgets/navbar_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
 
 class WorkerScreen extends StatefulWidget {
   const WorkerScreen({super.key});
@@ -10,15 +14,50 @@ class WorkerScreen extends StatefulWidget {
 }
 
 class _WorkerScreenState extends State<WorkerScreen> {
-  DateTime? _selectedDate;
+  final TextEditingController _dniController = TextEditingController();
 
+  Future<void> _loadWorkers(WorkerViewModel workerViewModel) async {
+    final dni = _dniController.text.trim(); // Get entered DNI
+    if (dni.isNotEmpty && dni.length == 8) {
+      try {
+        await workerViewModel.fetchWorkerDni(dni);
+      } catch (e) {
+        print("Error: Error al obtener los trabajadores: $e");
+      }
+    }
+  }
+
+  Future<void> _handleQrScan(WorkerViewModel workerViewModel) async {
+    await QRScanner.scanQRCode(
+      context: context,
+      onCodeScanned: (String code) async {
+        if (code.isNotEmpty) {
+          if (code.length == 8 && int.tryParse(code) != null) {
+            _dniController.text = code;  // Update the DNI controller with the scanned code
+            await _loadWorkers(workerViewModel);  // Load workers with the scanned DNI
+          }
+        }
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final workerViewModel = Provider.of<WorkerViewModel>(context, listen: false);
+      workerViewModel.clearWorkers();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final workerViewModel = Provider.of<WorkerViewModel>(context);
+
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(60.0),
-        child: const TopNavBarScreen(),
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(60.0),
+        child: TopNavBarScreen(),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -26,7 +65,7 @@ class _WorkerScreenState extends State<WorkerScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "Reporte de Canasta entregas por Trabajador",
+              "Reporte de Canasta entregas por Responsable",
               style: GoogleFonts.raleway(
                 fontSize: 16,
                 letterSpacing: 0.22,
@@ -36,28 +75,39 @@ class _WorkerScreenState extends State<WorkerScreen> {
             ),
             const SizedBox(height: 10),
 
-            // TextField para escanear código QR
+            // TextField to scan or enter DNI
             TextField(
+              controller: _dniController,
               decoration: InputDecoration(
                 labelText: "Buscar por DNI",
-                labelStyle: GoogleFonts.raleway(fontSize: 14, fontWeight: FontWeight.w600),
-                prefixIcon: const Icon(Icons.qr_code_scanner),
+                labelStyle: GoogleFonts.raleway(
+                    fontSize: 14, fontWeight: FontWeight.w600),
+                prefixIcon: IconButton(
+                  icon: const Icon(Icons.qr_code_scanner),
+                  onPressed: () => _handleQrScan(workerViewModel),
+                ),
                 prefixIconConstraints: const BoxConstraints(
                   minWidth: 40,
                   minHeight: 40,
                 ),
                 border: const OutlineInputBorder(),
-                contentPadding: const EdgeInsets.symmetric(vertical: 22.0, horizontal: 16.0),
+                contentPadding: const EdgeInsets.symmetric(
+                    vertical: 22.0, horizontal: 16.0),
               ),
+              onChanged: (value) {
+                if (value.length == 8) {
+                  _loadWorkers(workerViewModel);
+                }
+              },
             ),
-
             const SizedBox(height: 20),
 
+            // List of workers based on the selected date and DNI
             Expanded(
               child: ListView.builder(
-                padding: const EdgeInsets.only(top: 0),
-                itemCount: 1,
+                itemCount: workerViewModel.workers.length,
                 itemBuilder: (context, index) {
+                  final worker = workerViewModel.workers[index];
                   return Card(
                     elevation: 2,
                     margin: const EdgeInsets.symmetric(vertical: 4.0),
@@ -67,47 +117,53 @@ class _WorkerScreenState extends State<WorkerScreen> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            "Joys Navarro Adanaque",
+                            worker['FullName'] ?? 'Sin nombre',
                             style: GoogleFonts.raleway(
                               fontSize: 14,
                               fontWeight: FontWeight.w500,
                             ),
                           ),
-                          const SizedBox(height: 4), // Espacio entre el nombre y el DNI
+                          SizedBox(height: 4.0),
+                          // Mostrar la Planilla y FechaCreacion
                           Text(
-                            "DNI: 712349854",
+                            'Planilla: ${worker['Planilla'] ?? 'No disponible'}',
                             style: GoogleFonts.raleway(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
                               color: Colors.grey,
                             ),
                           ),
-                          const SizedBox(height: 8), // Espacio entre el DNI y la fecha de entrega
                           Text(
-                            "Fecha de entrega de canasta: 15/11/2024",
+                            'Fecha de entrega: ${worker['FechaCreacion'] ?? 'No disponible'}',
                             style: GoogleFonts.raleway(
-                              fontSize: 13,
+                              fontSize: 12,
                               fontWeight: FontWeight.w400,
-                              color: Colors.black87,
+                              color: Colors.grey,
                             ),
                           ),
-                          const SizedBox(height: 4), // Espacio entre la fecha de entrega y el entregado por
                           Text(
-                            "Entregado por: John Soto Navarro",
+                            'Responsable: ${worker['Responsable'] ?? 'No disponible'}',
                             style: GoogleFonts.raleway(
-                              fontSize: 13,
+                              fontSize: 12,
                               fontWeight: FontWeight.w400,
-                              color: Colors.black87,
+                              color: Colors.grey,
                             ),
                           ),
                         ],
+                      ),
+                      trailing: Text(
+                        'DNI: ${worker['DNI']}',
+                        style: GoogleFonts.raleway(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey,
+                        ),
                       ),
                     ),
                   );
                 },
               ),
             ),
-
           ],
         ),
       ),
